689e4f26823fc8edb348f917a7fa8f9f21690b07
jaustinjr-code <jaustinjr.collab@gmail.com>
2026-06-11 18:43:27 -0700

    Fix attendance icon

--- EmployeeAttendance/EmployeeAttendance/Features/Attendance/ContentView.swift
+++ EmployeeAttendance/EmployeeAttendance/Features/Attendance/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
             TabView(selection: $selectedTab) {
                 MainView()
                     .tabItem {
-                        Label(Tabs.attendance.rawValue, systemImage: "person.circle")
+                        Label(Tabs.attendance.rawValue, systemImage: "checkmark.circle")
                     }
 
                 DashboardView()
